a1d5ced4d5cdf5beecf911dc5919d19e6d113dbc
zaza <contact@zaza.com>
2020-05-05 21:20:26 -0500

    enviando el primer programa con cobol

---
+++ PRUEBA1.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author:RONY
+      * Date:05/05/2020
+      * Purpose:LEARNING CODE
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HOLAMUNDO.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "HOLA MUNDO CON COBOL"
+            STOP RUN.
+       END PROGRAM HOLAMUNDO.
